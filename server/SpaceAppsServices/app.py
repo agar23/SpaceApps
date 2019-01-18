8156b916c01bbe10d12eddca258ade4422240ad3
Alfonso <aaustinrivera@gmail.com>
2019-01-17 22:10:14 -0500

    Create baseline Flask services project

--- server/SpaceAppsServices/app.py
+++ server/SpaceAppsServices/app.py
@@ -1,24 +1,29 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, make_response
+from flask_restplus import Resource, Api
 from score_calculation import *
 import json
-
 import os, sys
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/start')
-def index():
-	return "Hello World!"
+@api.route('/whereToGo')                   
+class whereToGo(Resource):            
+    def get(self):                     
+        with open("./resources/score_list.json") as f:
+            data = json.load(f)
+            response = make_response(json.dumps(data))
+            response.headers['content-type'] = 'application/json'
+        return response
+        
+@api.route('/whereToCamp')                   
+class whereToGo(Resource):            
+    def get(self):                     
+        with open("./resources/vegetation_means.json") as f:
+            data = json.load(f)
+            response = make_response(json.dumps(data))
+            response.headers['content-type'] = 'application/json'
+        return response
 
-@app.route('/whereToGo', methods = ['GET'])
-def whereToGo():
-    with open("./resources/score_list.json") as f:
-        data = json.load(f)
-    return json.dumps(data)
-
-@app.route('/whereToCamp', methods = ['GET'])
-def whereToCamp():
-    with open("./resources/vegetation_means.json") as f:
-        data = json.load(f)
-    return json.dumps(data)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
